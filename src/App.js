9cb0d0318c78a41a2227e64be5cc221f579e4a4f
yamashi-ta-keshi <59694038+yamashi-ta-keshi@users.noreply.github.com>
2022-07-18 18:11:22 +0900

    add buttons with atomic design

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import "./styles.css";
 
 export default function App() {
@@ -5,6 +7,8 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
     </div>
   );
 }
